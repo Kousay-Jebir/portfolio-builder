963c54b72844fdb2597a4314185b9cdf19adfcd2
Kousay Jebir <kousayjebir@gmail.com>
2025-04-24 17:27:02 +0100

    adapt code structure to master branch

--- main-backend/src/api-documentation/api-documentation.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SwaggerDocumentationService } from './swagger/swagger-documentation.service';
-
-@Module({
-  providers: [SwaggerDocumentationService],
-  exports: [SwaggerDocumentationService]
-})
-export class ApiDocumentationModule { }
