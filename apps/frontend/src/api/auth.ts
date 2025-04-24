aaee37872ceddea7c629e013ccf247a2b9dec777
chedli01 <chedli.masmoudi01@gmail.com>
2025-04-24 20:18:23 +0100

    auth apis

---
+++ apps/frontend/src/api/auth.ts
@@ -0,0 +1,39 @@
+import axios from 'axios';
+import { axiosMain } from './axios';
+
+export const register = async (userData: {
+  username: string;
+  email: string;
+  password: string;
+}) => {
+  axiosMain
+    .post('/auth/register', userData)
+    .then((res) => {
+      return res.data;
+    })
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
+};
+
+export const login = async (userCredentials: {
+  email: string;
+  password: string;
+}) => {
+  axiosMain
+    .post('/auth/login', userCredentials)
+    .then((res) => {
+      return res.data;
+    })
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
+};
+
+export const loginWithGoogle = async ()=>{
+    axiosMain.get('/auth/google').then((res)=>{return res.data} ).catch((err)=>{console.log(err);
+        throw err
+    })
+}
