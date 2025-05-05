1e47c051c1fb2c8e4d91f98752f625aded731800
chedli01 <chedli.masmoudi01@gmail.com>
2025-05-05 11:15:23 +0100

    fix pagination dto

--- apps/consult-app/src/user/consult-user.controller.ts
+++ apps/consult-app/src/user/consult-user.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get, Param, Query, UseGuards } from '@nestjs/common';
 import { ConsultUserService } from './consult-user.service';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiQuery, ApiTags } from '@nestjs/swagger';
 import { BlacklistGuard, JwtAuthGuard } from '@portfolio-builder/shared';
 import { PaginationDto } from 'libs/shared/src/pagination/dto/pagination.dto';
 @ApiTags('users')
@@ -9,6 +9,7 @@ export class ConsultUserController {
 constructor(private readonly consultUserService : ConsultUserService){}
   @UseGuards(JwtAuthGuard,BlacklistGuard)
   @ApiBearerAuth('JWT-auth')
+   
   @Get()
   async getUsers(@Query() pagination : PaginationDto) {
     return await this.consultUserService.getUsersWithPortfolio(pagination);
--- libs/shared/src/pagination/dto/pagination.dto.ts
+++ libs/shared/src/pagination/dto/pagination.dto.ts
@@ -1,11 +1,21 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { Type } from "class-transformer";
 import { IsNumber, IsOptional } from "class-validator";
 
 export class PaginationDto{
 
+    @ApiProperty({required:false})
+
     @IsOptional()
+    @Type(() => Number)
+
     @IsNumber()
     offset?:number
+    @ApiProperty({required:false})
+
     @IsOptional()
+    @Type(() => Number)
+
     @IsNumber()
     limit?:number
 }
