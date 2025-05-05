642c7f5bb987eec4140d6a397313e00d8fc20d35
chedli01 <chedli.masmoudi01@gmail.com>
2025-05-05 15:53:44 +0100

    create activity type enum

---
+++ apps/consult-app/src/activitylog/enum/activity-type.enum.ts
@@ -0,0 +1,6 @@
+export enum ActivityTypeEnum {
+    VIEW='VIEW',
+    SEARCH='SEARCH',
+    CLICK='CLICK',
+    LIKE='LIKE'
+}
--- apps/consult-app/src/activitylog/schema/activity-log.schema.ts
+++ apps/consult-app/src/activitylog/schema/activity-log.schema.ts
@@ -1,5 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document, Types } from 'mongoose';
+import { ActivityTypeEnum } from '../enum/activity-type.enum';
 
 export type ActivityLogDocument = ActivityLog & Document;
 
@@ -8,8 +9,8 @@ export class ActivityLog {
   @Prop({ type: Types.ObjectId, ref: 'User', required: true })
   user: Types.ObjectId;
 
-  @Prop({ required: true })
-  type: 'VIEW' | 'SEARCH' | 'CLICK' | 'LIKE';
+  @Prop({ required: true,type:String,enum:ActivityTypeEnum })
+  type: ActivityTypeEnum;
 
   @Prop({ type: Object })
   metadata: Record<string, any>;
