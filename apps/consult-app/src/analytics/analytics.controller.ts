e309e6d48c1ddaed58bf0e1df105e72bd079f1a0
chedli01 <chedli.masmoudi01@gmail.com>
2025-05-08 00:23:23 +0100

    fix analytics controller

--- apps/consult-app/src/analytics/analytics.controller.ts
+++ apps/consult-app/src/analytics/analytics.controller.ts
@@ -2,31 +2,33 @@ import { Controller, Get, Query, UseGuards } from '@nestjs/common';
 import { AnalyticsService } from './analytics.service';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { ActivityTypeEnum } from '../activitylog/enum/activity-type.enum';
-import { BlacklistGuard, ConnectedUser, JwtAuthGuard } from '@portfolio-builder/shared';
+import {
+  BlacklistGuard,
+  ConnectedUser,
+  JwtAuthGuard,
+} from '@portfolio-builder/shared';
 @ApiTags('analytics')
 @Controller('analytics')
 export class AnalyticsController {
-    constructor(private readonly analyticsService : AnalyticsService){}
+  constructor(private readonly analyticsService: AnalyticsService) {}
 
-    @Get('most-viewed-portfolios')
-    async getMostViewedPortfolios(){
-        return await this.analyticsService.getPortfoliosOwners(ActivityTypeEnum.VIEW)
-        
-    }
+  @Get('most-viewed-portfolios')
+  async getMostViewedPortfolios() {
+    return await this.analyticsService.getPortfoliosOwners(
+      ActivityTypeEnum.VIEW,
+    );
+  }
 
-    @Get('most-liked-portfolios')
-    async getMostLikedPortfolios(){
-        return await this.analyticsService.getPortfoliosOwners(ActivityTypeEnum.LIKE)
-
-    }
-   @UseGuards(JwtAuthGuard,BlacklistGuard)
-   @ApiBearerAuth('JWT-auth')
-    @Get('recently-viewed')
-    async getRecentlyViewed(@ConnectedUser() user : any){
-        return await this.analyticsService.getRecentlyViewed(user.id)
-
-    }
-
-
-
+  @Get('most-liked-portfolios')
+  async getMostLikedPortfolios() {
+    return await this.analyticsService.getPortfoliosOwners(
+      ActivityTypeEnum.LIKE,
+    );
+  }
+  @UseGuards(JwtAuthGuard, BlacklistGuard)
+  @ApiBearerAuth('JWT-auth')
+  @Get('recently-viewed')
+  async getRecentlyViewed(@ConnectedUser() user: any) {
+    return await this.analyticsService.getRecentlyViewed(user.id);
+  }
 }
