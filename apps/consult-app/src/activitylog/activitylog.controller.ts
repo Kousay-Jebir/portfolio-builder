20c74778937560f78031fbfecd7da03dac7b593b
chedli01 <chedli.masmoudi01@gmail.com>
2025-05-07 22:20:24 +0100

    getActivit(y endpoint

--- apps/consult-app/src/activitylog/activitylog.controller.ts
+++ apps/consult-app/src/activitylog/activitylog.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth } from '@nestjs/swagger';
+import { BlacklistGuard, ConnectedUser, JwtAuthGuard } from '@portfolio-builder/shared';
+import { ActivitylogService } from './activitylog.service';
 
-@Controller('activitylog')
-export class ActivitylogController {}
+@Controller('activity')
+export class ActivitylogController {
+    constructor(private readonly activityLogService : ActivitylogService){}
+    @UseGuards(JwtAuthGuard,BlacklistGuard)
+    @ApiBearerAuth('JWT-auth')
+    @Get()
+    async getMyActivity(@ConnectedUser() user : any){
+
+        return await this.activityLogService.getUserActivity(user.id)
+
+
+    }
+
+}
--- apps/consult-app/src/activitylog/activitylog.module.ts
+++ apps/consult-app/src/activitylog/activitylog.module.ts
@@ -3,9 +3,10 @@ import { ActivitylogService } from './activitylog.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ActivityLog, ActivityLogSchema } from './schema/activity-log.schema';
 import { ActivitylogController } from './activitylog.controller';
+import { TokenModule } from '@portfolio-builder/shared';
 
 @Module({
-  imports:[MongooseModule.forFeature([{ name: ActivityLog.name, schema: ActivityLogSchema }])],
+  imports:[MongooseModule.forFeature([{ name: ActivityLog.name, schema: ActivityLogSchema }]),TokenModule],
   providers: [ActivitylogService],
   exports : [ActivitylogService],
   controllers: [ActivitylogController]
