c273db638a4ffb45c0a1720248ea36d1d2596f9a
chedli01 <chedli.masmoudi01@gmail.com>
2025-05-10 20:50:24 +0100

    format data

--- apps/consult-app/src/activitylog/activitylog.service.ts
+++ apps/consult-app/src/activitylog/activitylog.service.ts
@@ -6,83 +6,88 @@ import { ActivityTypeEnum } from './enum/activity-type.enum';
 
 @Injectable()
 export class ActivitylogService {
-    constructor(
-        @InjectModel(ActivityLog.name)
-        private readonly activityLogModel: Model<ActivityLogDocument>,
-      ) {}
-    
-      async logActivity(userId: string, type: ActivityTypeEnum, metadata: Record<string, any>) {
-        return this.activityLogModel.create({
+  constructor(
+    @InjectModel(ActivityLog.name)
+    private readonly activityLogModel: Model<ActivityLogDocument>,
+  ) {}
+
+  async logActivity(
+    userId: string,
+    type: ActivityTypeEnum,
+    metadata: Record<string, any>,
+  ) {
+    return this.activityLogModel.create({
+      user: userId,
+      type,
+      metadata,
+    });
+  }
+
+  async getUserActivity(userId: string, limit?: number) {
+    const query = this.activityLogModel
+      .find({ user: userId })
+      .sort({ createdAt: -1 });
+    if (limit) {
+      query.limit(limit);
+    }
+
+    return query.exec();
+  }
+
+  async getMostPortfolioOwnerIds(
+    type: ActivityTypeEnum,
+  ): Promise<{ ownerId: string; count: number }[]> {
+    const results = await this.activityLogModel.aggregate([
+      {
+        $match: { type: type },
+      },
+      {
+        $group: {
+          _id: '$metadata.ownerId',
+          count: { $sum: 1 },
+        },
+      },
+      {
+        $sort: { count: -1 },
+      },
+      {
+        $project: {
+          _id: 0,
+          ownerId: '$_id',
+          count: 1,
+        },
+      },
+    ]);
+
+    return results;
+  }
+
+  async getRecentViews(userId: string) {
+    const logs = await this.activityLogModel.find({ user: userId });
+    const owners = logs.map((item) => {
+      if (item.type == ActivityTypeEnum.VIEW) return item.metadata.ownerId;
+    });
+    return owners;
+  }
+
+  async getMostSearchedCategory(userId: string): Promise<string | null> {
+    const result = await this.activityLogModel.aggregate([
+      {
+        $match: {
+          type: ActivityTypeEnum.SEARCH,
           user: userId,
-          type,
-          metadata,
-        });
-      }
-    
-      async getUserActivity(userId: string,) {
-        return await this.activityLogModel
-          .find({ user: userId})
-          .sort({ createdAt: -1 })
-          .exec();
-      }
+        },
+      },
+      {
+        $group: {
+          _id: '$metadata.category',
+          count: { $sum: 1 },
+        },
+      },
+      { $sort: { count: -1 } },
+      { $limit: 1 },
+    ]);
 
-      async getMostPortfolioOwnerIds(type:ActivityTypeEnum): Promise<{ ownerId: string; count: number }[]> {
-        const results = await this.activityLogModel.aggregate([
-          {
-            $match: { type: type }
-          },
-          {
-            $group: {
-              _id: '$metadata.ownerId',
-              count: { $sum: 1 }
-            }
-          },
-          {
-            $sort: { count: -1 }
-          },
-          {
-            $project: {
-              _id: 0,
-              ownerId: '$_id',
-              count: 1
-            }
-          }
-        ]);
-      
-        return results;
-      }
-
-      async getRecentViews(userId : string){
-        const logs = await this.activityLogModel.find({user:userId})
-        const owners= logs.map((item)=>{
-          if(item.type==ActivityTypeEnum.VIEW)
-          return item.metadata.ownerId
-
-        })
-        return owners
-      }
-
-      async getMostSearchedCategory(userId: string): Promise<string | null> {
-        const result = await this.activityLogModel.aggregate([
-          {
-            $match: {
-              type: ActivityTypeEnum.SEARCH,
-              user: userId
-            }
-          },
-          {
-            $group: {
-              _id: '$metadata.category',
-              count: { $sum: 1 }
-            }
-          },
-          { $sort: { count: -1 } },
-          { $limit: 1 }
-        ]);
-      
-        return result[0]?._id || null;
-      }
-      
-      
-      
+    return result[0]?._id || null;
+  }
 }
