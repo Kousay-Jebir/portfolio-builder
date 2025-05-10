ab36b45ce17ce4d2b0f120e0b75219cc3778391c
Kousay Jebir <kousayjebir@gmail.com>
2025-05-10 16:50:41 +0100

    for later use : observe main area width

--- apps/frontend/src/main.jsx
+++ apps/frontend/src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { UIProvider } from './DrawerContext.jsx'
 import "./index.css"
+import { CanvasProvider } from './builder/layout-engine/utils/contexts/CanvasContexdt.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <UIProvider>
-      <App />
+      <CanvasProvider>
+        <App />
+      </CanvasProvider>
     </UIProvider>
   </StrictMode>,
 )
